7a5f4c0c86fa9e19813171859d94da0328622a72
nicolas gorosito <nicolas.goro123@gmail.com>
2025-04-17 12:05:12 -0300

    primeros pasos en el proyecto

---
+++ archivos secundarios/main.js
@@ -0,0 +1,149 @@
+
+const nombre=prompt("ingrese su nombre completo: ")
+console.log(nombre)
+
+const contraseña=prompt("ingrese su contraseña: ")
+console.log(contraseña)
+
+let codigozaptillas= prompt("cual es el codigo de la zapatillas que estas buscando")
+console.log(codigozaptillas)
+
+//catalogo:
+let codigos = [
+    o01= {
+        codigo: "o01",
+        nombre : "dunk low",
+        color : "violeta",
+        talle : "36",
+    },
+    o02= {
+        codigo: "o02",
+        nombre : "dunk low",
+        color : "violeta",
+        talle : "36",
+    },
+    o03 = {
+        codigo: "o03",
+        nombre : "travisXnike",
+        color : "rosa",
+        talle : "37",
+    },
+    o04 = {
+        codigo:"o04",
+        nombre : "botines de tusal",
+        color : "azul",
+        talle : "38",
+    },
+    o05 = {
+        codigo:"o05",
+        nombre : "botines de 11",
+        color : "rojo",
+        talle : "39",
+    }
+]
+
+
+// const zapatillas = [o01,o02,o03,o04,o05]
+// console.log(zapatillas)
+
+// if(codigozaptillas in zapatillas){
+//     console.log(zapatillas[codigozaptillas])
+// }
+// else{
+//     console.log("no hay mas stock de esas zapatillas")
+// }
+
+// // esta funcion itera entre las zapatillas que hay y devuelve con texto personalizado la informacion de las zapatillas
+// for(const zapatilla of zapatillas){
+//     console.log("el codigo de tu zapatilla es: " + zapatilla.codigo + ", el nombre de tu zapatilla es: " + zapatilla.nombre + ", el color de tu zapatilla es: "+ zapatilla.color + ", el talle de tu zapatilla es: " + zapatilla.talle)
+// }
+
+// function sumar (){
+//     let numeroA = 15
+//     let numeroB = 10
+//     let resultado = numeroA + numeroB
+
+//     return resultado
+// }
+// esta es una calculadora de iva
+// let calculo = sumar()
+// let calculoConIVA = calculo*1.21
+// console.log(calculoConIVA)
+
+// function calculadoraCIva(numeroIngresado){
+//     console.log(numeroIngresado*1.21)
+// }
+// calculadoraCIva(10)
+
+// const descuentos=[
+//     descuento1= 0.90,
+//     descuento2= 0.80,
+//     descuento3= 0.70,
+// ]
+
+// const productos =[
+//     remera = 50,
+//     zapatilla =100,
+//     buzo =75,
+// ]
+
+// const prodCliente = prompt("que producto queres comprar: ")
+
+// if (prodCliente in productos){
+//     console.log("tenemos stock de " + prodCliente)
+// } 
+
+
+// const pdescuento = prompt("que descuento le queres aplicar: ")
+// if(pdescuento in descuentos){
+//     console.log("podemos aplicar ese descuento")
+// }
+
+
+
+
+// for (descuento in descuentos){
+//     if (pdescuento === "10%" ){
+//         console.log("el precio final le quedaria en: " + productos*descuento1)
+//     }
+//     else if(pdescuento === "20%"){
+//         console.log("el precio final le quedaria en: " + zapátillas*descuento2)
+//     }
+//     else if(pdescuento === "30%"){
+//         console.log("el precio final le quedaria en: " + zapatillas*descuento3)
+//     }
+//     else{
+//         console.log("no podemos aplicar este descuento")
+//     }
+// }
+
+
+
+// function descuentoA(descuento1){
+//     pdescuento = 0.9
+//     console.log(descuento1*pdescuento)
+// }
+// descuentoA(50)
+
+// function descuentoB(descuento1){
+//     pdescuento = 0.8
+//     console.log(descuento1*pdescuento)
+// }
+// descuentoB(50)
+
+// function descuentoC(descuento1){
+//     pdescuento = 0.7
+//     console.log(descuento1*pdescuento)
+// }
+// descuentoC(50)
+
+// function descuentoD(descuento1){
+//     pdescuento = 0.6
+//     console.log(descuento1*pdescuento)
+// }
+// descuentoD(50)
+
+
+
+
+
